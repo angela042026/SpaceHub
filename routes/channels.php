d648da2cdaf75255a6350723310758b2b9c36ae6
Eduardo <edjpereira@gmail.com>
2026-07-15 12:04:16 +0100

    Melhoria em routes/channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+// Canal público
+Broadcast::channel('chat', function () {
+    return true;
+});
